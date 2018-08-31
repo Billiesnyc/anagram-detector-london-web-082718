d114ccaadada876df394e0daad89684b70572e54
Billie Simmons <billiesnyc@gmail.com>
2018-08-31 15:48:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    answer_holder = []
+    array.each do |answer| 
+      if answer.split("").sort == @word.split("").sort
+        answer_holder.push(answer)
+      end
+    end
+    answer_holder
+  end
+  
+end
